d09e0703579dce1a98d7d711ea488abac1c963b0
rodolfoHOk <rodolfo.okino@gmail.com>
2020-09-16 20:29:19 -0300

    primeiras alterações para funcionar com  o front end

--- src/main/java/br/com/hioktec/minhasfinancas/api/dto/UsuarioDTO.java
+++ src/main/java/br/com/hioktec/minhasfinancas/api/dto/UsuarioDTO.java
@@ -1,11 +1,15 @@
 package br.com.hioktec.minhasfinancas.api.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class UsuarioDTO {
 	
